e3b882367b202c1f8cfb275f5b7f55c118d5b28c
Nejc Pintar <nejc@poison.si>
2024-05-07 19:42:38 +0200

    tracking response public

--- Sources/flightbuddies-dto-swift/TrackingResponse.swift
+++ Sources/flightbuddies-dto-swift/TrackingResponse.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-struct TrackingResponse: Codable {
-    let nearbyPilots: [User.View]
-    let audioMessages: [AudioMessage.View]
+public struct TrackingResponse: Codable {
+    public let nearbyPilots: [User.View]
+    public let audioMessages: [AudioMessage.View]
+
+    public init(nearbyPilots: [User.View], audioMessages: [AudioMessage.View]) {
+        self.nearbyPilots = nearbyPilots
+        self.audioMessages = audioMessages
+    }
 }
