8c46bbbb23f287e01ec61611e6e8261808479143
Nejc Pintar <nejc@poison.si>
2024-05-07 20:03:45 +0200

    public inits

--- Sources/flightbuddies-dto-swift/AudioMessage.swift
+++ Sources/flightbuddies-dto-swift/AudioMessage.swift
@@ -11,12 +11,23 @@ public struct AudioMessage {
     public struct Create: Codable {
         public let kind: Kind
         public let audio: Data
+
+        public init(kind: Kind, audio: Data) {
+            self.kind = kind
+            self.audio = audio
+        }
     }
 
     public struct View: Codable {
         public let kind: Kind
         public let audio: Data
         public let senderId: UUID
+
+        public init(kind: Kind, audio: Data, senderId: UUID) {
+            self.kind = kind
+            self.audio = audio
+            self.senderId = senderId
+        }
     }
 
     public enum Kind: Codable {
