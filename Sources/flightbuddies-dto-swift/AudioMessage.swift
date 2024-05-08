78acb3ecc576e76b5bf59f3543ec4b7033788db0
Nejc Pintar <nejc@poison.si>
2024-05-08 22:13:13 +0200

    added pilot name to audiomessage

--- Sources/flightbuddies-dto-swift/AudioMessage.swift
+++ Sources/flightbuddies-dto-swift/AudioMessage.swift
@@ -21,11 +21,13 @@ public struct AudioMessage {
     public struct View: Codable {
         public let kind: Kind
         public let audio: Data
+        public let senderName: String
         public let senderId: UUID
 
-        public init(kind: Kind, audio: Data, senderId: UUID) {
+        public init(kind: Kind, audio: Data, senderName: String, senderId: UUID) {
             self.kind = kind
             self.audio = audio
+            self.senderName = senderName
             self.senderId = senderId
         }
     }
